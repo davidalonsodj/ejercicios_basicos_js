bf5fc97bf18cb2d11a607d464c7ef896bed656ae
davidalonsodj <davidalonsodj@gmail.com>
2025-01-20 14:21:54 +0100

    EJERCICIOS
    EJERCICIOS

---
+++ ejercicio2.js
@@ -0,0 +1,15 @@
+const jedi = {nombre: "Luke Skywalker", edad: 19};
+
+// Tu código para cambiar la edad aquí...
+
+jedi.edad =25;
+
+console.log (jedi);
+
+const nombre ="Leia";
+const apellido ="Organa";
+const edad= 20;
+
+const mensaje = "Soy " + nombre + " " + apellido + ", tengo " + edad + " años y soy una princesa de Alderaan";
+
+console.log(mensaje)
---
+++ ejercicio3.js
@@ -0,0 +1,8 @@
+const sable1 = {nombre: "Shoto de Yoda", precio: 1500};
+const sable2 = {nombre: "Sable de Darth Vader", precio: 2000};
+
+// Tu código para calcular el precio total aquí...
+
+const preciototal = sable1.precio + sable2.precio;
+
+console.log(preciototal)
