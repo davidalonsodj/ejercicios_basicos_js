3539a2bd063754dc291a8cd0f64f803df02b734a
davidalonsodj <davidalonsodj@gmail.com>
2025-03-21 09:58:31 +0100

    EJERCICIO 4

--- ejercicio4.js
+++ ejercicio4.js
@@ -8,5 +8,28 @@ const aldeanos = ["Fibrilio", "Narciso", "Vacarena", "Tendo", "Nendo"];
 //4.6 - Imprime por consola el último elemento de este array sin atacar a la posición explicitamente
 // Pista: puedes usar el método length con algo más para ello.
 
+
+// 4.1
 console.log(aldeanos[3]);
-aldeanos.push("Cervasio")
+
+// 4.2
+aldeanos.push("Cervasio");
+console.log(aldeanos);
+
+// 4.3
+aldeanos[0] = "Bambina";
+console.log(aldeanos[0]);
+
+// 4.4
+const aldeanosReverso = [...aldeanos].reverse();
+console.log(aldeanosReverso);
+
+// 4.5
+const indexNarciso = aldeanos.indexOf("Narciso");
+if (indexNarciso !== -1) {
+  aldeanos.splice(indexNarciso, 1, "Canela");
+}
+console.log(aldeanos);
+
+// 4.6
+console.log(aldeanos[aldeanos.length - 1]);
