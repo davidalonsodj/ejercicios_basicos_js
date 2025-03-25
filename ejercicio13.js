29645809ad60214e877a2e7fbcaf25355f5000d1
davidalonsodj <davidalonsodj@gmail.com>
2025-03-25 16:38:02 +0100

    Update ejercicio13.js

--- ejercicio13.js
+++ ejercicio13.js
@@ -11,6 +11,12 @@ const names = [
     'Jessica',
     'Marc'
   ];
-  function nameFinder(nameList) {
-    // Completar
+  function nameFinder(nameList, buscarnombre) {
+    const index = nameList.indexOf(buscarnombre);
+    if (index !== -1) {
+      return { found: true, position: index };
+    } else {
+      return { found: false };
+    }
   }
+  console.log(nameFinder(names, 'Tony')); 
