412f3f9f558dfd804ca1b57d47845448c5f4b233
davidalonsodj <davidalonsodj@gmail.com>
2025-03-26 15:30:10 +0100

    15 16 17

---
+++ ejercicio15.js
@@ -0,0 +1,5 @@
+const products = ['Camiseta de Metallica', 'Pantalón vaquero', 'Gorra de beisbol', 'Camiseta de Basket', 'Cinturón de Orión', 'AC/DC Camiseta'];
+for (const item of products) {
+  if (item.includes(`Camiseta`)){
+    console.log(item);
+}}
